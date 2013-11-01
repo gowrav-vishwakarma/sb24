5e051cb4870be03c2c9ce16e0324ee0ad1d1eabb
Gowrav Vishwakarma <gowrav@xserver.(none)>
2013-11-01 14:32:03 +0530

    forgot password completed

--- lib/Form.php
+++ lib/Form.php
@@ -1,7 +1,7 @@
 <?php
 
 class Form extends Form_Basic{
-	function addSubmit($label='Save',$name=null,$spot='form_buttons',$template='button'){
+	function addSubmit($label='Save',$name=null,$spot='form_buttons',$template=array('button','button')){
         $submit = $this->add('Form_Submit',$name,$spot,$template)
             ->setLabel($label)
             ->setNoSave();
