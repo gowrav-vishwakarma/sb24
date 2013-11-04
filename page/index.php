942a9461a9e9ade959f53cc7bf6b5e5c7cba1111
Gowrav Vishwakarma <gowrav@gowrav-Lenovo-G570.(none)>
2013-11-04 09:32:38 +0530

    work transfer from layyp to server

--- page/index.php
+++ page/index.php
@@ -9,10 +9,8 @@ class page_index extends page_base_site {
 		$this->api->template->trySet('center_span',12);
 		$this->api->template->tryDel('content_class');
 		$this->api->template->tryDel('body_margin');
-		// $cols=$this->add('Columns');
-		// $col1=$cols->addColumn(1)->add('View')->setHtml('&nbsp');
-		// $col2=$cols->addColumn(5);//->addStyle(array("margin-top"=>"150px","margin-left"=>"450px"));
-		// $col3=$cols->addColumn(2);
+		
+
 		$this->add('HtmlElement')->setElement('img')->setAttr('src',$this->api->sb24_config['front_image'])->setAttr('align','center');
 		$search_form=$this->add('Form')->addClass('stacked');
 		$search_form->addField('line','search','')->addClass('span10');
@@ -22,25 +20,24 @@ class page_index extends page_base_site {
 		}
 
 		$cols=$this->add('Columns')->addClass('right-front-page-col');
-		$col_login=$cols->addColumn(5);
-		$col_register=$cols->addColumn(7)->addClass('col');//->addClass('right-front-page-col');
-		// ->setStyle(array('margin-left'=>'36.5%','background-color'=>'white','border'=>'10px solid burlywood','border-radius'=>'40px'));
+		$col_login=$cols->addColumn(8);
+		// $col_register=$cols->addColumn(7)->addClass('col');
 
 
-		$col_register->add('H3')->set('Register Now, Its Free ...')
-			->sub('Add your own free listings, get unlimited informations ... ')->setStyle('color','burlywood');
+		// $col_register->add('H3')->set('Register Now, Its Free ...')
+		// 	->sub('Add your own free listings, get unlimited informations ... ')->setStyle('color','burlywood');
 
-		$model=$this->add('Model_Member');
-		$model->getElement('password')->system(true);
+		// $model=$this->add('Model_Member');
+		// $model->getElement('password')->system(true);
 
-		$form_register = $col_register->add('Form');
-		$form_register->setModel($model,'base');
-		$form_register->addSubmit("Register")->addClass('shine');
+		// $form_register = $col_register->add('Form');
+		// $form_register->setModel($model,'base');
+		// $form_register->addSubmit("Register")->addClass('shine');
 
-		if($form_register->isSubmitted()){
-			$form_register->update();
-			$form_register->js(null, $this->js()->univ()->redirect('memberpanel_page_dashboard'))->univ()->closeDialog()->execute();
-		}
+		// if($form_register->isSubmitted()){
+		// 	$form_register->update();
+		// 	$form_register->js(null, $this->js()->univ()->redirect('memberpanel_page_dashboard'))->univ()->closeDialog()->execute();
+		// }
 
 
 		$col_login->add('H3')->set('Login to Your Account')->setStyle('color','burlywood');
