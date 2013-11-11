c424a43eff9a141b9bc62ba673eca081911bb625
Gowrav Vishwakarma <gowrav@gowrav-Lenovo-G570.(none)>
2013-11-11 18:57:50 +0530

    working on menu and ui corrected

--- lib/Menu.php
+++ lib/Menu.php
@@ -1,6 +1,29 @@
 <?php
 
 class Menu extends Menu_Basic{
+	function addMenuItem($page,$label=null){
+        if(!$label){
+            $label=ucwords(str_replace('_',' ',$page));
+        }
+        $id=$this->name.'_i'.count($this->items);
+        $label=$this->api->_($label);
+        $js_page=null;
+        if($page instanceof jQuery_Chain){
+            $js_page="#";
+            $this->js('mouseover',$page)->_selector('#'.$id);
+            $this->js('mouseout','$(".ui-dialog-content").dialog("close")')->_selector('.ui-dialog-content');
+            $page=$id;
+        }
+        $this->items[]=array(
+            'id'=>$id,
+            'page'=>$page,
+            'href'=>$js_page?:$this->api->url($page),
+            'label'=>$label,
+            $this->class_tag=>$this->isCurrent($page)?$this->current_menu_class:$this->inactive_menu_class,
+        );
+        return $this;
+    }
+
 	function isCurrent($href){
         // returns true if item being added is current
         $href = explode("&", $href);
--- page/base/site.php
+++ page/base/site.php
@@ -7,9 +7,15 @@ class page_base_site extends page_base_null {
 	}
 
 	function setUpSiteMenus(){
-		$this->api->menu
+		$sb=$this->api->menu
 					->addMenuItem('index','Home')
-					->addMenuItem($this->api->url('history_page_search',array('reset'=>1)),'History')
+					->addMenuItem($this->api->url('history_page_search',array('reset'=>1)),'History');
+		$sb=$this->api->menu->addSubMenu('sdfsd','sdfdsf');
+					$sb->addMenuItem('sdfsd','p1');
+					$sb->addMenuItem('sdfsd','p2');
+					$sb->addMenuItem('sdfsd','p3');
+					
+		$this->api->menu
 					->addMenuItem($this->api->url('businessdirectory_page_search',array('reset'=>1)),'Free Business Listing')
 					->addMenuItem($this->api->url('socialdirectory_page_search',array('reset'=>1)),'Telephone Web Directory')
 					->addMenuItem($this->api->url('distance_page_search',array('reset'=>1)),'Distance')
