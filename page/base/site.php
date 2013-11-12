60230068b72cb9fa63ffb4083032a33a5027ee94
Gowrav Vishwakarma <gowrav@xserver.(none)>
2013-11-12 19:54:48 +0530

    SB24 working almost final

--- page/base/site.php
+++ page/base/site.php
@@ -7,27 +7,32 @@ class page_base_site extends page_base_null {
 	}
 
 	function setUpSiteMenus(){
-		$sb=$this->api->menu
-					->addMenuItem('index','Home')
-					->addMenuItem($this->api->url('history_page_search',array('reset'=>1)),'History');
-		$sb=$this->api->menu->addSubMenu('sdfsd','sdfdsf');
-					$sb->addMenuItem('sdfsd','p1');
-					$sb->addMenuItem('sdfsd','p2');
-					$sb->addMenuItem('sdfsd','p3');
-					
-		$this->api->menu
-					->addMenuItem($this->api->url('businessdirectory_page_search',array('reset'=>1)),'Free Business Listing')
-					->addMenuItem($this->api->url('socialdirectory_page_search',array('reset'=>1)),'Telephone Web Directory')
-					->addMenuItem($this->api->url('distance_page_search',array('reset'=>1)),'Distance')
-					->addMenuItem($this->api->url('tracker_page_search',array('reset'=>1)),'Tracker')
-					->addMenuItem($this->api->url('emergency_page_search',array('reset'=>1)),'Emergency Numbers')
-					->addMenuItem($this->api->url('event_page_search',array('reset'=>1)),'Events')
-					->addMenuItem($this->api->url('jobandvacancy_page_search',array('reset'=>1)),'Jobs & Vacancy')
-					->addMenuItem($this->api->url('blooddoner_page_serach',array('reset'=>1)),'Blood Doner')
-					->addMenuItem($this->api->url('salesandpurchase_page_search',array('reset'=>1)),'Sales & Purchase')
-					->addMenuItem($this->api->url('memberpanel_page_dashboard',array('reset'=>1)),'My Account')
-					;
+		$this->api->menu->destroy();
+		$this->api->add('View',null,'Menu',array('view/sitemenu'));
 	}
+
+	// function setUpSiteMenus(){
+	// 	$sb=$this->api->menu
+	// 				->addMenuItem('index','Home')
+	// 				->addMenuItem($this->api->url('history_page_search',array('reset'=>1)),'History');
+	// 	$sb=$this->api->menu->addSubMenu('sdfsd','sdfdsf');
+	// 				$sb->addMenuItem('sdfsd','p1');
+	// 				$sb->addMenuItem('sdfsd','p2');
+	// 				$sb->addMenuItem('sdfsd','p3');
+					
+	// 	$this->api->menu
+	// 				->addMenuItem($this->api->url('businessdirectory_page_search',array('reset'=>1)),'Free Business Listing')
+	// 				->addMenuItem($this->api->url('socialdirectory_page_search',array('reset'=>1)),'Telephone Web Directory')
+	// 				->addMenuItem($this->api->url('distance_page_search',array('reset'=>1)),'Distance')
+	// 				->addMenuItem($this->api->url('tracker_page_search',array('reset'=>1)),'Tracker')
+	// 				->addMenuItem($this->api->url('emergency_page_search',array('reset'=>1)),'Emergency Numbers')
+	// 				->addMenuItem($this->api->url('event_page_search',array('reset'=>1)),'Events')
+	// 				->addMenuItem($this->api->url('jobandvacancy_page_search',array('reset'=>1)),'Jobs & Vacancy')
+	// 				->addMenuItem($this->api->url('blooddoner_page_serach',array('reset'=>1)),'Blood Doner')
+	// 				->addMenuItem($this->api->url('salesandpurchase_page_search',array('reset'=>1)),'Sales & Purchase')
+	// 				->addMenuItem($this->api->url('memberpanel_page_dashboard',array('reset'=>1)),'My Account')
+	// 				;
+	// }
 
 	function setUpAdds(){
 		$top_blocks = $this->add('adds/Model_AdBlock')
